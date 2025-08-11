dbdd9f20e247db68b38d848c40373b3dd48b52e8
Ziya Ul Rehman <ziyaulrehman3@gmail.com>
2025-08-12 01:00:13 +0530

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,16 @@ import About from "@/components/About";
 import Services from "@/components/Services";
 import Feedback from "@/components/Feedback";
 import JoinUs from "@/components/JoinUs";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="w-full overflow-hidden">
+      <Head>
+        <title>Salon Staff Provider</title>
+        <meta name="description" content="Salon Staff Provider" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <Hero />
       <About />
       <Services />
